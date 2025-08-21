84b163cc23b8932b77a12cda718048db64da4941
eduayslan <eduardoayslan10@gmail.com>
2025-08-21 14:07:42 -0300

    Implementação dos Dashboard's

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Ambiente;
+use App\Models\Registro;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AmbienteSeeder::class,
             SensorSeeder::class,
+            RegistroSeeder::class
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Livewire\Ambiente\AmbienteCreate;
 use App\Livewire\Ambiente\AmbienteEdit;
 use App\Livewire\Ambiente\AmbienteList;
+use App\Livewire\Dashboard;
 use App\Models\Ambiente;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/create', AmbienteCreate::class)->name('ambientes.create');
-Route::get('/', AmbienteList::class)->name('ambientes.list');
-Route::get('/{id}/edit', AmbienteEdit::class)->name('ambientes.edit');
+Route::get('/', Dashboard::class);
